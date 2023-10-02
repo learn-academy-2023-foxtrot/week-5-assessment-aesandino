42f60c64b8cc8f3fe646437ab1cedc80fff50332
aesandino <anabellasandino@gmail.com>
2023-10-02 05:53:38 -0700

    completed assessment

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -14,6 +14,16 @@
 
 // a) Create a test with expect statements using the variables provided.
 
+describe("convertedStrings", () => {
+    it("returns a coded message", () => {
+    expect(convertedStrings(secretCodeWord1)).toEqual("L4ck4d4isic4l")
+    expect(convertedStrings(secretCodeWord2)).toEqual("G0bbl3dyg00k")
+    expect(convertedStrings(secretCodeWord3)).toEqual("3cc3ntr1c")
+
+    })
+})
+
+
 const secretCodeWord1 = "Lackadaisical"
 // Expected output: "L4ck4d41s1c4l"
 const secretCodeWord2 = "Gobbledygook"
@@ -23,11 +33,30 @@ const secretCodeWord3 = "Eccentric"
 
 // b) Create the function that makes the test pass.
 
+const convertedStrings = (string) => {
+    return string.replaceAll('a', 4/ 'e' 3/ 'i', 1/ 'o', 0)
+}
+console.log(convertedStrings(string))
+
+
 // Pseudo code:
+
+// input: String
+// output: a different string (with converted letters)
+// process: 
+// -create a function that is called convertedStrings and the parameter is the strings. Using the ".replaceAll" method to affect all of the vowels listed in each string
 
 // --------------------2) Create a function that takes in an array of words and a single letter and returns an array of all the words containing that particular letter.
 
 // a) Create a test with expects statement using the variable provided.
+
+describe("filteredFruits", () => {
+    it("returns an array of all the words containing that particular letter", () => {
+    expect(filteredFruits(filterLetterA)).toEqual(["Mango", "Apricot", "Peach"])
+    expect(filteredFruits(filterLetterE)).toEqual(["Cherry", "Blueberry", "Peach"])
+
+    })
+})
 
 const fruitArray = ["Mango", "Cherry", "Apricot", "Blueberry", "Peach", "Kiwi"]
 
@@ -37,12 +66,33 @@ const filterLetterE = "e"
 // Expected output: ["Cherry", "Blueberry", "Peach"]
 
 // b) Create the function that makes the test pass.
+const filteredFruits = (array) => {
+    return array.sort("a", "e")
+}
+console.log(filteredFruits(array))
+
 
 // Pseudo code:
+// input: an array of words and a single letter 
+// output: an array of all the words containing that particular letter
+// process: -create a function that is called "filteredFruits" and the parameter is the arrays of strings...Using the "sort()" method to filter the strings that only contain the specified letters. 
+
 
 // --------------------3) Create a function that takes in an array of 5 numbers and determines whether or not the array is a "full house". A full house is exactly one pair and one three of a kind.
 
 // a) Create a test with expect statements using the variable provided.
+
+describe("potentialFullHouse", () => {
+    it("returns a true or false statement that determines whether or not the array is a full house", () => {
+    expect(potentialFullHouse(hand1)).toEqual("true")
+    expect(potentialFullHouse(hand2)).toEqual("false")
+    expect(potentialFullHouse(hand3)).toEqual("false")
+    expect(potentialFullHouse(hand4)).toEqual("true")
+
+
+    })
+})
+
 
 const hand1 = [5, 5, 5, 3, 3]
 // Expected output: true
@@ -55,4 +105,12 @@ const hand4 = [7, 2, 7, 2, 7]
 
 // b) Create the function that makes the test pass.
 
+const potentialFullHouse = (hand1, hand2, hand3, hand4) => {
+    return array.some([] > 3)
+}
+console.log(potentialFullHouse.every(array))
+
 // Pseudo code:
+// input: an array of 5 numbers
+// output: true or false response based on whether or not the array of numbers equates to a "full house". 
+// process: -Using "every()" method(so that it affects each array element)...Using "some()" method to define T or F for each array
--- ruby-challenges.rb
+++ ruby-challenges.rb
@@ -11,6 +11,22 @@ filter_letter_t = 't'
 # Expected output: ['tea', 'water', 'soda water']
 
 # Pseudo code:
+# input: strings that represent beverages
+# output: strings that contain the specified vowel
+# process: creating a new method name: drinks, using the select() method to be sure that the output contains either 'o' or 't'. The include() method looks at each element in the array. 
+
+def 
+   drinks.select.includes(('o', 't') && ['coffee', 'tea', 'juice', 'water', 'soda water'])
+end
+
+p drinks.includes['o']
+p drinks.includes['t']
+
+
+
+
+
+
 
 # -------------------2) Create a method that takes in a hash and returns one array with all the hash values at their own index and in alphabetical order. No nested arrays. Use the test variable provided.
 # HINT: Google 'ruby get rid of nested arrays'
@@ -19,12 +35,39 @@ us_states = { northwest: ['Washington', 'Oregon', 'Idaho'], southwest: ['Califor
 # Expected output: ['Arizona', 'California', 'Idaho', 'Maine', 'Nevada', 'New Hampshire', 'Oregon', 'Rhode Island', 'Washington'] 
 
 # Pseudo code:
+# input: hash
+# output: an arry that alphabetizes all of the hashes
+# process: from https://www.educative.io/answers/how-to-flatten-multi-dimensional-arrays-into-one-in-ruby: .flatten -  
+# An array that is not one-dimensional can be flattened in Ruby using the flatten method. It recursively flattens the array and returns a new array that is one-dimensional(one dimensional equates to no arrays nested). ALso using the "sort_by" method to sort the elements in the array by the 1st index value of each element. Attempted to use the alphabetize method - probably not used correctly (so that the states are listed in alphabetical order)
+
+class States
+    def 
+        us_states = { northwest: ['Washington', 'Oregon', 'Idaho'], southwest: ['California', 'Arizona', 'Nevada'], notheast: ['Maine', 'New Hampshire', 'Rhode Island'] }
+        
+    end 
+    puts alphabetize us_states
+    p us_states.sort_by([1]).flatten
+
 
 # --------------------3a) Create a class called Bike that is initialized with a model, wheels, and current_speed. The default number of wheels is 2. The current_speed should start at 0. Create a bike_info method that returns a sentence with all the data from the bike object.
 
 # Expected output example: 'The Trek bike has 2 wheels and is going 0 mph.'
 
 # Pseudo code:
+# process: listing out each of the pieces of data that need to be initialized, including the model, wheels, and current_speed
+
+class Bike
+    def initialize(:model, :wheels, :current_speed)
+        @model = model
+        @wheels = 2
+        @current_speed = 0  
+
+    end 
+
+    bike_info = {model: 'The Trek bike', wheels: '2', current_speed: '0'}
+
+    bike_info do |model, wheels, current_speed|
+    p "#{model} has #{wheels} and is going #{current_speed} mph."
 
 # -------------------3b) Add the ability to pedal faster and brake. The pedal_faster method should increase the speed by a given amount. The brake method should decrease the speed by a given amount. The bike cannot go negative speeds.
 
@@ -34,3 +77,10 @@ us_states = { northwest: ['Washington', 'Oregon', 'Idaho'], southwest: ['Califor
 # Expected output example: my_bike.brake(25) => 0
 
 # Pseudo code:
+
+# process: defining the class name, defining each of the pedaling and braking limits, defining method for pedaling faster and braking
+
+class Bike
+    def
+    pedal_faster = {my_bike.pedal_faster: '(10) => 10', my_bike.pedal_faster: '(18) => 28', my_bike.brake: '(5) => 23', my_bike.brake: '(25) => 0'}
+    end 
